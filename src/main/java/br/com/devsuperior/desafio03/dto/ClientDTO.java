e36c0aaa8ba31c60c62746af34662169df4dbb28
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-14 16:19:48 -0300

    adicionado o getter na classe DTO pois não funcionou com a notation @Getter na classe.

--- src/main/java/br/com/devsuperior/desafio03/dto/ClientDTO.java
+++ src/main/java/br/com/devsuperior/desafio03/dto/ClientDTO.java
@@ -7,13 +7,33 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @NoArgsConstructor
-@Getter
+
 public class ClientDTO {
     private Long id;
     private String name;
     private String cpf;
     private LocalDate birthDate;
     private Integer children;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
 
     public ClientDTO(Client entity) {
         id = entity.getId();
