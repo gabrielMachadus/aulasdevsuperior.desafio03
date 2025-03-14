315b07c3d2314d4100dd259370dfada9e9e65133
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-14 14:29:53 -0300

    Criação da classe ClientService com metodo findById.

---
+++ src/main/java/br/com/devsuperior/desafio03/services/ClientService.java
@@ -0,0 +1,18 @@
+package br.com.devsuperior.desafio03.services;
+
+import br.com.devsuperior.desafio03.dto.ClientDTO;
+import br.com.devsuperior.desafio03.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Transactional
+    public ClientDTO findById(Long id){
+        return new ClientDTO(clientRepository.findById(id).get());
+    }
+}
