3b1064ed77b9c23cf8f09227f96e82268b2d9a34
Machado, Gabriel <gabrielgamerlive@gmail.com>
2025-03-14 10:37:36 -0300

    primeiro commit

---
+++ src/test/java/br/com/devsuperior/desafio03/Desafio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.desafio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
