956bebc2cf44b8de0e945fe8a6c2579565651ab8
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-14 14:28:01 -0300

    criação do metodo findById para classe ClientController

--- src/main/java/br/com/devsuperior/desafio03/entities/controllers/ClientController.java
+++ src/main/java/br/com/devsuperior/desafio03/entities/controllers/ClientController.java
@@ -1,25 +1,21 @@
 package br.com.devsuperior.desafio03.entities.controllers;
 
-import br.com.devsuperior.desafio03.entities.Client;
-import br.com.devsuperior.desafio03.repositories.ClientRepository;
+import br.com.devsuperior.desafio03.dto.ClientDTO;
+import br.com.devsuperior.desafio03.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 
     @Autowired
-    private ClientRepository clientRepository;
+    private ClientService clientService;
 
-    @GetMapping
-    public List<Client> list() {
-        List<Client> clients = clientRepository.findAll();
-        return clients.stream().toList();
+    @GetMapping(value = "/{id}")
+    public ClientDTO findById(@PathVariable Long id){
+        ClientDTO clientDTO = clientService.findById(id);
+        return clientDTO;
     }
 
 
