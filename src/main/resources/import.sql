94e459b4a884d8cb2a40fe0137da32794fd10a9a
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2025-03-14 19:54:54 -0300

    retirado ID do seed para gerar automático pelo banco e adicionado campo Income.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,20 @@
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (1, 'Ana Souza', '123.456.789-01', '1990-05-12', 2);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Ana Souza', '123.456.789-01', '1990-05-12', 2,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (2, 'Carlos Pereira', '234.567.890-12', '1985-08-25', 1);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Carlos Pereira', '234.567.890-12', '1985-08-25', 1,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (3, 'Mariana Lima', '345.678.901-23', '1992-11-03', 0);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Mariana Lima', '345.678.901-23', '1992-11-03', 0,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (4, 'João Silva', '456.789.012-34', '1980-02-17', 3);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('João Silva', '456.789.012-34', '1980-02-17', 3,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (5, 'Fernanda Costa', '567.890.123-45', '1995-09-10', 1);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Fernanda Costa', '567.890.123-45', '1995-09-10', 1,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (6, 'Ricardo Santos', '678.901.234-56', '1978-07-22', 2);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Ricardo Santos', '678.901.234-56', '1978-07-22', 2,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (7, 'Luciana Almeida', '789.012.345-67', '1989-12-30', 0);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Luciana Almeida', '789.012.345-67', '1989-12-30', 0,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (8, 'Gabriel Rocha', '890.123.456-78', '1993-04-15', 1);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Gabriel Rocha', '890.123.456-78', '1993-04-15', 1,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (9, 'Paula Martins', '901.234.567-89', '1983-06-28', 2);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Paula Martins', '901.234.567-89', '1983-06-28', 2,10000.00);
 
-INSERT INTO tb_client (id, name, cpf, birth_date, children) VALUES (10, 'Felipe Oliveira', '012.345.678-90', '1997-01-05', 0);
+INSERT INTO tb_client (name, cpf, birth_date, children, income) VALUES ('Felipe Oliveira', '012.345.678-90', '1997-01-05', 0,10000.00);
+
